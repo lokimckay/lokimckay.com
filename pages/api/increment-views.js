0653dc9126d81bbb4dfdb79c985d3b5f2d9bc714
Lachlan McKay <sneyed@gmail.com>
2020-09-06 02:49:28 +1000

    Add Sentry (#9)
    * Add Sentry

--- pages/api/increment-views.js
+++ pages/api/increment-views.js
@@ -1,23 +1,41 @@
 import db from "../../lib/db-admin";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const incrementViews = async (req, res) => {
-  if (!req.query.id) {
-    return res.status(400).json({
-      error: 'Missing "id" query parameter',
-    });
-  }
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    tracesSampleRate: process.env.SENTRY_SAMPLE_RATE || 0,
+  });
 
-  const ref = db.ref("views").child(req.query.id);
-  const { snapshot } = await ref.transaction(currentViews => {
-    if (currentViews === null) {
-      return 1;
+  const transaction = Sentry.startTransaction({
+    op: "increment-views",
+    name: "increment-views",
+  });
+
+  try {
+    if (!req.query.id) {
+      return res.status(400).json({
+        error: 'Missing "id" query parameter',
+      });
     }
-    return currentViews + 1;
-  });
 
-  return res.status(200).json({
-    total: snapshot.val(),
-  });
+    const ref = db.ref("views").child(req.query.id);
+    const { snapshot } = await ref.transaction(currentViews => {
+      if (currentViews === null) {
+        return 1;
+      }
+      return currentViews + 1;
+    });
+
+    return res.status(200).json({
+      total: snapshot.val(),
+    });
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
 };
 
 export default incrementViews;
