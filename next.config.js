0ac3376677290437e9a7c7f653d7deb8a0b37c75
sneyed <sneyed@gmail.com>
2020-09-06 02:46:05 +1000

    Add Sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 const mdxPrism = require("mdx-prism");
 const withMdxEnhanced = require("next-mdx-enhanced");
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GITLAB_COMMIT_SHA,
+  VERCEL_BITBUCKET_COMMIT_SHA,
+} = process.env;
+
+const COMMIT_SHA =
+  VERCEL_GITHUB_COMMIT_SHA ||
+  VERCEL_GITLAB_COMMIT_SHA ||
+  VERCEL_BITBUCKET_COMMIT_SHA;
+
+process.env.SENTRY_DSN = SENTRY_DSN;
+const basePath = "";
 
 module.exports = withMdxEnhanced({
+  // MDX config
   layoutPath: "components/post",
   defaultLayout: true,
   fileExtensions: ["mdx"],
@@ -11,4 +32,37 @@ module.exports = withMdxEnhanced({
     process: (mdxContent, frontMatter) => {},
     phase: "prebuild|loader|both",
   },
-})(/* your normal nextjs config */);
+})(
+  // Regular config
+  {
+    serverRuntimeConfig: {
+      rootDir: __dirname,
+    },
+    webpack: (config, options) => {
+      if (!options.isServer) {
+        config.resolve.alias["@sentry/node"] = "@sentry/browser";
+      }
+
+      if (
+        SENTRY_DSN &&
+        SENTRY_ORG &&
+        SENTRY_PROJECT &&
+        SENTRY_AUTH_TOKEN &&
+        COMMIT_SHA &&
+        NODE_ENV === "production"
+      ) {
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            include: ".next",
+            ignore: ["node_modules"],
+            stripPrefix: ["webpack://_N_E/"],
+            urlPrefix: `~${basePath}/_next`,
+            release: COMMIT_SHA,
+          })
+        );
+      }
+      return config;
+    },
+    basePath,
+  }
+);
