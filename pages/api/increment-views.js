9902cf9a3fabb0ee0c50cfe1410dbd1cfbaa715f
Loki <sneyed@gmail.com>
2021-01-18 02:13:58 +1000

    Don't increment views when running locally

--- pages/api/increment-views.js
+++ pages/api/increment-views.js
@@ -2,6 +2,11 @@ import { getDb } from "../../lib/firebase/admin";
 
 const incrementViews = async (req, res) => {
   try {
+    if (process.env.DEBUG_MODE)
+      return res
+        .status(403)
+        .json({ error: "Can't increment views in debug environment" });
+
     if (!req.query.id)
       return res.status(400).json({
         error: 'Missing "id" query parameter',
