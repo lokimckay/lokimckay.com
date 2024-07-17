8bfac4c6ed9f02bb88be758a173ac01aae8235b6
Loki <lokimckay@gmail.com>
2024-07-17 20:24:01 +1000

    delete all

--- components/post/mdx/quote.js
+++
@@ -1,3 +0,0 @@
-export const blockquote = props => {
-  return <blockquote className="quote" {...props} />;
-};
--- pages/posts/index.js
+++
@@ -1,6 +0,0 @@
-import { frontMatter as posts } from "./**/*.mdx";
-import PostList from "../../components/postList";
-
-const Index = () => <PostList posts={posts} />;
-
-export default Index;
