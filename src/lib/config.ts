97c9f8fc6ae14e630b5e9564318fdd1805addb80
Loki <lokimckay@gmail.com>
2024-08-04 16:56:15 +1000

    add astro starter template

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,10 @@ export const favicon = "😈";
 export const siteName = "Loki McKay";
 export const gcpBaseUrl = "https://storage.cloud.google.com/lokimckay-com/";
 
-export const pinnedPosts = ["godot-rapier-3d", "civitai-model-browser", "bull"];
+export const pinnedPosts = [
+  "astro-starter",
+  "preact-skeletons",
+  "godot-rapier-3d",
+  "civitai-model-browser",
+  "bull",
+];
