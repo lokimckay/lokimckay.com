2aa891ac6094d06b6d7182809f1f7a368b6c7dd5
Loki <lokimckay@gmail.com>
2024-08-07 23:37:45 +1000

    add skeletons post and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import transformers from "./src/lib/shikiTransformers";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: "https://ltm.bio/",
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "night-owl",
